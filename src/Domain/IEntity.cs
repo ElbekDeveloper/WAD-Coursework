e7dd0477ceeafb0bf16a1193da11d4031b6293fb
Elbek <52498074+ElbekDeveloper@users.noreply.github.com>
2021-02-18 15:23:52 +0500

    Change access modifier

--- src/Domain/IEntity.cs
+++ src/Domain/IEntity.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Domain
+﻿namespace Domain
 {
-    interface IEntity<TId>
+    public interface IEntity<TId>
     {
         TId Id { get; set; }
     }
