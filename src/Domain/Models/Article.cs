075871ec22b4d790b9c79a4588178e651efd39f1
Elbek <52498074+ElbekDeveloper@users.noreply.github.com>
2021-02-18 14:47:44 +0500

    Create domain models

---
+++ src/Domain/Models/Article.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Domain.Models
+{
+    public class Article : IEntity<int>
+    {
+        public int Id { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public Author Author { get; set; }
+    }
+}
---
+++ src/Domain/Models/Tag.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Domain.Models
+{
+    public class Tag : IEntity<int>
+    {
+        public int Id { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        public string Name { get; set; }
+        public IList<Article> Articles { get; set; }
+    }
+}
