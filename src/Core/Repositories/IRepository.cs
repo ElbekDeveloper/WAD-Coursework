a61edb897975af9482e5e381e5502d38feb27f8b
Elbek <52498074+ElbekDeveloper@users.noreply.github.com>
2021-02-18 15:24:16 +0500

    Declare the contract of functionalities

--- src/Core/Repositories/IRepository.cs
+++ src/Core/Repositories/IRepository.cs
@@ -1,7 +1,24 @@
-﻿namespace Core.Repositories
-{
-    public interface IRepository<TEntity> where TEntity : class
-    {
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading;
+using System.Threading.Tasks;
 
+namespace Core.Repositories
+{
+    public interface IRepository<TModel, TId> where TModel : IEntity<TId>
+    {
+        Task AddAsync(TModel obj, CancellationToken cancellationToken = default);
+        Task<IEnumerable<TModel>> GetAllAsync(CancellationToken cancellationToken = default);
+        Task<TModel> GetByIdAsync(TId id, CancellationToken cancellationToken = default);
+        Task UpdateAsync(TId id, TModel obj, CancellationToken cancellationToken = default);
+        Task<TModel> RemoveAsync(TId id, CancellationToken cancellationToken = default);
+        /// <summary>
+        /// Finds list of objects based on the predicate
+        /// </summary>
+        /// <param name="predicate">Lambda expression to filter objects</param>
+        /// <returns></returns>
+        Task<IEnumerable<TModel>> Find(Expression<Func<TModel, bool>> predicate);
     }
 }
