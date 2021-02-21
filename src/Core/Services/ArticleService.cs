f9b5c48448958b1d48f978751b75b5f1977943d2
Elbek <52498074+ElbekDeveloper@users.noreply.github.com>
2021-02-21 16:46:36 +0500

    Add implementation of articles service interface

---
+++ src/Core/Services/ArticleService.cs
@@ -0,0 +1,25 @@
+﻿using Core.Interfaces;
+using Core.Repositories;
+using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Core.Services
+{
+    public class ArticleService : IArticleService
+    {
+        private IArticleRepository _repository;
+
+        public ArticleService(IArticleRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<IEnumerable<Article>> GetArticles()
+        {
+            return await _repository.GetAllAsync();
+        }
+    }
+}
