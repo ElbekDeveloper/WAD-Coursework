0d4d4ebd005719273df10838d0ba0a7eea1e4c69
Elbek <52498074+ElbekDeveloper@users.noreply.github.com>
2021-02-21 13:22:04 +0500

    Add list of Tags column

--- src/Domain/Models/Article.cs
+++ src/Domain/Models/Article.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -19,5 +20,6 @@ namespace Domain.Models
         [ForeignKey("Author")]
         public int AuthorId { get; set; }
         public virtual Author Author { get; set; }
+        public ICollection<Tag> Tags { get; set; }
     }
 }
