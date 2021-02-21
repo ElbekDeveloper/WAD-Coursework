4262293e76ec381d48cf3022b00c529a4ba9a471
Elbek <52498074+ElbekDeveloper@users.noreply.github.com>
2021-02-21 16:46:19 +0500

    Add articles service

---
+++ src/Core/Interfaces/IArticleService.cs
@@ -0,0 +1,12 @@
+﻿
+using Domain.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Core.Interfaces
+{
+    public  interface IArticleService
+    {
+        Task<IEnumerable<Article>> GetArticles();
+    }
+}
