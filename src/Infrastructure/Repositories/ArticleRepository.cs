6e16ab346e6cb32cf0e72fb229dbe7a7280e1ef1
Elbek <52498074+ElbekDeveloper@users.noreply.github.com>
2021-02-21 16:47:34 +0500

    Add article repository

---
+++ src/Infrastructure/Repositories/ArticleRepository.cs
@@ -0,0 +1,27 @@
+﻿using Core.Repositories;
+using Domain.Models;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public class ArticleRepository : Repository<Article>, IArticleRepository
+    {
+        public ArticleRepository(ApplicationDbContext dbContext) : base(dbContext)
+        {
+
+        }
+
+        public override async Task<IEnumerable<Article>> GetAllAsync(CancellationToken cancellationToken = default)
+        {
+            var articles = await _dbContext.Articles
+                                     .Include(a => a.Author)
+                                     .Include(a => a.Tags).ToListAsync(cancellationToken);
+
+            return articles;
+        }
+    }
+}
