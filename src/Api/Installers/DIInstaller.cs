8e775edf6e031d3c354df33ea8e231d3419f1d78
Elbek <52498074+ElbekDeveloper@users.noreply.github.com>
2021-02-21 16:44:20 +0500

    Add dependeny injection registerer

---
+++ src/Api/Installers/DIInstaller.cs
@@ -0,0 +1,18 @@
+﻿using Core.Interfaces;
+using Core.Repositories;
+using Core.Services;
+using Infrastructure.Repositories;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Api.Installers
+{
+    public class DIInstaller : IInstaller
+    {
+        public void InstallServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddTransient<IArticleService, ArticleService>();
+            services.AddTransient<IArticleRepository, ArticleRepository>();
+        }
+    }
+}
