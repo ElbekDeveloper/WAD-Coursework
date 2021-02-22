713fae417a7054f8bdba1e533c05663e75f6bb6c
Elbek <52498074+ElbekDeveloper@users.noreply.github.com>
2021-02-22 14:36:52 +0500

    Implement post method

--- src/Core/Services/ArticleService.cs
+++ src/Core/Services/ArticleService.cs
@@ -1,7 +1,10 @@
-﻿using Core.Interfaces;
+﻿using AutoMapper;
+using Core.Interfaces;
 using Core.Repositories;
+using Core.Resources;
 using Domain.Models;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
@@ -11,15 +14,25 @@ namespace Core.Services
     public class ArticleService : IArticleService
     {
         private IArticleRepository _repository;
+        private readonly IMapper _mapper;
 
-        public ArticleService(IArticleRepository repository)
+        public ArticleService(IArticleRepository repository, IMapper mapper)
         {
             _repository = repository;
+            _mapper = mapper;
         }
 
-        public async Task<IEnumerable<Article>> GetArticles()
+        public async Task<IEnumerable<ArticleResource>> GetArticles()
         {
-            return await _repository.GetAllAsync();
+            var article = await _repository.GetAllAsync();
+            return _mapper.Map<IEnumerable<ArticleResource>>(article);
+        }
+
+        public async Task<ArticleResource> AddArticle(ArticleResource articleResource, CancellationToken cancellationToken)
+        {
+            var article = _mapper.Map<Article>(articleResource);
+            var createdArticle = await _repository.AddAsync(article, cancellationToken);
+            return _mapper.Map<ArticleResource>(createdArticle);
         }
     }
 }
