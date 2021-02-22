80ad12ed97a05d7ad88e79a8c632e9503d6d8807
Elbek <52498074+ElbekDeveloper@users.noreply.github.com>
2021-02-22 18:25:07 +0500

    Add new resource for post requests

---
+++ src/Core/Resources/AddArticleResource.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Core.Resources
+{
+    public class AddArticleResource
+    {
+        [Required]
+        [MaxLength(2000)]
+        public string Title { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        [Required]
+        public string Body { get; set; }
+        public int AuthorId { get; set; }
+        public ICollection<int> Tags { get; set; }
+        public AddArticleResource()
+        {
+            Tags = new Collection<int>();
+        }
+    }
+}
