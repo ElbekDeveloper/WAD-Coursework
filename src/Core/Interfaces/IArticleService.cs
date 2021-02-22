90544e8ba6fc4d4d9e5c6e3d987809a26776b608
Elbek <52498074+ElbekDeveloper@users.noreply.github.com>
2021-02-22 14:36:42 +0500

    Declare post method

--- src/Core/Interfaces/IArticleService.cs
+++ src/Core/Interfaces/IArticleService.cs
@@ -1,12 +1,15 @@
 ﻿
+using Core.Resources;
 using Domain.Models;
 using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Core.Interfaces
 {
     public  interface IArticleService
     {
-        Task<IEnumerable<Article>> GetArticles();
+        Task<IEnumerable<ArticleResource>> GetArticles();
+        Task<ArticleResource> AddArticle(ArticleResource articleResource, CancellationToken cancellationToken= default);
     }
 }
