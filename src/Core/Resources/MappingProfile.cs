fe28631d95716bf7d6736d10dc43fae023a264fe
Elbek <52498074+ElbekDeveloper@users.noreply.github.com>
2021-02-25 17:25:08 +0500

    Add extra mapping configuration

--- src/Core/Resources/MappingProfile.cs
+++ src/Core/Resources/MappingProfile.cs
@@ -9,11 +9,13 @@ namespace Core.Resources
         {
             //Domain to Api Resources
             CreateMap<Article, ArticleResource>();
-            CreateMap<Author, AuthorResource>();
-            CreateMap<Tag, TagResource>();
+            CreateMap<Author, AuthorResource>().ReverseMap();
+
 
             //From Resource to Domain model
-
+            CreateMap<AddArticleResource, Article>();
+            CreateMap<ArticleResource, Article>()
+                .ForMember(a => a.AuthorId, opt => opt.MapFrom(ar => ar.Author.Id));
         }
     }
 }
