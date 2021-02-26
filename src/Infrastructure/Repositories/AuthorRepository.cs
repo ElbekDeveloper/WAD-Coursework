96182eaee3007e0b483ac9913ba9aeb303b52c72
Elbek <52498074+ElbekDeveloper@users.noreply.github.com>
2021-02-26 14:27:46 +0500

    Implement CRUD for author model

---
+++ src/Infrastructure/Repositories/AuthorRepository.cs
@@ -0,0 +1,17 @@
+﻿using Core.Repositories;
+using Domain.Models;
+using Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Repositories
+{
+    public class AuthorRepository : Repository<Author>, IAuthorRepository
+    {
+        public AuthorRepository(ApplicationDbContext dbContext):base(dbContext)
+        {
+
+        }
+    }
+}
