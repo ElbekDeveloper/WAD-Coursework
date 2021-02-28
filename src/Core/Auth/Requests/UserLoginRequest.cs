24c83a7c858ab026accea0d6ae1f949fd6e8598a
Elbek <52498074+ElbekDeveloper@users.noreply.github.com>
2021-02-28 10:24:20 +0500

    Add login model

---
+++ src/Core/Auth/Requests/UserLoginRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Auth.Requests
+{
+    public class UserLoginRequest
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
