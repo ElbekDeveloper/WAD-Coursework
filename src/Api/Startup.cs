6e4ff21bffe5f92192780a02d985fb29cb6ffae3
Elbek <52498074+ElbekDeveloper@users.noreply.github.com>
2021-03-02 17:21:35 +0500

    Add authorization

--- src/Api/Startup.cs
+++ src/Api/Startup.cs
@@ -58,7 +58,7 @@ namespace Api
             app.UseStaticFiles();
 
             app.UseAuthentication();
-
+            app.UseAuthorization();
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGet("/", context =>
