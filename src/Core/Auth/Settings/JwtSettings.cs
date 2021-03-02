2ae736d361bc6f85db332df55358244ad3125160
Elbek <52498074+ElbekDeveloper@users.noreply.github.com>
2021-03-02 19:38:13 +0500

    Add lifetime

--- src/Core/Auth/Settings/JwtSettings.cs
+++ src/Core/Auth/Settings/JwtSettings.cs
@@ -1,9 +1,12 @@
-﻿namespace Core.Auth.Settings
+﻿using System;
+
+namespace Core.Auth.Settings
 {
     public class JwtSettings
     {
         public string Secret { get; set; }
         public string Audience { get; set; }
         public string Issuer { get; set; }
+        public TimeSpan TokenLifetime { get; set; }
     }
 }
