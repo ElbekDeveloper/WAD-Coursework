0fadd03e347ac65258c8fc912f45396162eb3574
Elbek <52498074+ElbekDeveloper@users.noreply.github.com>
2021-03-02 17:19:01 +0500

    Update author resource to add to db

--- src/Core/Resources/AddAuthorResource.cs
+++ src/Core/Resources/AddAuthorResource.cs
@@ -2,8 +2,7 @@
 {
     public class AddAuthorResource
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public string Name { get; set; }
         public string Email { get; set; }
     }
 }
