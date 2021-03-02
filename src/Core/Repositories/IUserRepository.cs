266015131826fd47e1f61beae236115a616e34f2
Elbek <52498074+ElbekDeveloper@users.noreply.github.com>
2021-03-02 17:16:41 +0500

    Declare user repository logic

---
+++ src/Core/Repositories/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using Core.Auth.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Threading.Tasks;
+
+namespace Core.Repositories
+{
+    public interface IUserRepository 
+    {
+        Task<AuthResult> CreateAsync(string email, string password);
+        Task<AuthResult> LoginAsync(string email, string password);
+        Task<bool> CheckUserOwnsArticle(int articleId, string userId);
+        
+    }
+}
