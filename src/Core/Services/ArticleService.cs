be12b998a52d0e17af2f10797ac603fbe899cdb1
Elbek <52498074+ElbekDeveloper@users.noreply.github.com>
2021-03-02 17:18:12 +0500

    Update add logic

--- src/Core/Services/ArticleService.cs
+++ src/Core/Services/ArticleService.cs
@@ -12,48 +12,51 @@ namespace Core.Services
 {
     public class ArticleService : IArticleService
     {
-        private IArticleRepository _repository;
+        private IArticleRepository _articleRepository;
+        private IUserRepository _userRepository;
         private readonly IMapper _mapper;
 
-        public ArticleService(IArticleRepository repository, IMapper mapper)
+        public ArticleService(IArticleRepository repository,IUserRepository userRepository, IMapper mapper)
         {
-            _repository = repository;
+            _userRepository = userRepository;
+            _articleRepository = repository;
             _mapper = mapper;
         }
 
         public async Task<IEnumerable<ArticleResource>> GetArticles()
         {
-            var article = await _repository.GetAllAsync();
+            var article = await _articleRepository.GetAllAsync();
             return _mapper.Map<IEnumerable<ArticleResource>>(article);
         }
 
-        public async Task<ArticleResource> AddArticle(AddArticleResource addArticleResource, CancellationToken cancellationToken)
+        public async Task<ArticleResource> AddArticle(string userId, AddArticleResource addArticleResource, CancellationToken cancellationToken)
         {
+ 
             var article = _mapper.Map<Article>(addArticleResource);
             article.CreatedDate = DateTime.UtcNow;
-            var createdArticle = await _repository.AddAsync(article, cancellationToken);
+            article.AuthorId = userId;
+            var createdArticle = await _articleRepository.AddAsync(article, cancellationToken);
             return _mapper.Map<ArticleResource>(createdArticle);
         }
 
         public async Task<ArticleResource> DeleteArticle(int id, CancellationToken cancellationToken = default)
         {
-           var article = await _repository.RemoveAsync(id, cancellationToken);
+           var article = await _articleRepository.RemoveAsync(id, cancellationToken);
             return _mapper.Map<ArticleResource>(article);
         }
 
         public async Task<ArticleResource> GetArticle(int id, CancellationToken cancellationToken = default)
         {
-            var article = await _repository.GetByIdAsync(id, cancellationToken);
+            var article = await _articleRepository.GetByIdAsync(id, cancellationToken);
             return _mapper.Map<ArticleResource>(article);
         }
 
         public async Task<ArticleResource> UpdateArticle(int id,AddArticleResource articleResource, CancellationToken cancellationToken = default)
         {
-           
             var article = _mapper.Map<Article>(articleResource);
             article.Id = id;
             article.UpdatedDate = DateTime.UtcNow;
-            await _repository.UpdateAsync(article, cancellationToken);
+            await _articleRepository.UpdateAsync(article, cancellationToken);
             return await GetArticle(article.Id, cancellationToken);
         }
     }
