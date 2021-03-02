6fdfaeb18c4a7602d00caf9eafea66eca45ecb79
Elbek <52498074+ElbekDeveloper@users.noreply.github.com>
2021-03-02 17:20:00 +0500

    Add Id getter method

---
+++ src/Core/Auth/Extensions/UserExtension.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Linq;
+
+namespace Core.Auth.Extensions
+{
+    public static class UserExtension
+    {
+        public static string GetUserId(this HttpContext httpContext)
+        {
+            if (httpContext.User is null)
+            {
+                return string.Empty;
+            }
+            return httpContext.User.Claims.Single(x => x.Type == "id").Value;
+        }
+    }
+}
