31798b921a91fa30f9788cc48d75d48b20031d93
Elbek <52498074+ElbekDeveloper@users.noreply.github.com>
2021-03-17 17:56:31 +0500

    Update logic for author object

--- src/Core/Services/AuthorService.cs
+++ src/Core/Services/AuthorService.cs
@@ -2,8 +2,7 @@
 using Core.Interfaces;
 using Core.Repositories;
 using Core.Resources;
-using Domain.Models;
-using System;
+using Microsoft.AspNetCore.Identity;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
@@ -13,49 +12,32 @@ namespace Core.Services
 
     public class AuthorService : IAuthorService
     {
-        private IAuthorRepository _repository;
+        private IAuthorRepository<IdentityUser> _repository;
         private readonly IMapper _mapper;
 
 
-        public AuthorService(IAuthorRepository repository, IMapper mapper)
+        public AuthorService(IAuthorRepository<IdentityUser> repository, IMapper mapper)
         {
             _repository = repository;
             _mapper = mapper;
         }
 
-        public async Task<AuthorResource> AddAuthor(AddAuthorResource authorResource, CancellationToken cancellationToken = default)
+        
+        public async Task<AuthorResource> DeleteAuthor(string id, CancellationToken cancellationToken = default)
         {
-            var author = _mapper.Map<Author>(authorResource);
-            var result = await _repository.AddAsync(author, cancellationToken);
-            return _mapper.Map<AuthorResource>(result);
-        }
-
-        public async Task<AuthorResource> DeleteAuthor(int id, CancellationToken cancellationToken = default)
-        {
-           var deletedAuthor = await _repository.RemoveAsync(id, cancellationToken);
+           var deletedAuthor = await _repository.DeleteAuthorAsync(id, cancellationToken);
             return _mapper.Map<AuthorResource>(deletedAuthor);
         }
 
-        public async Task<AuthorResource> GetAuthor(int id, CancellationToken cancellationToken = default)
+        public async Task<IEnumerable<AuthorResource>> GetAuthors(CancellationToken cancellationToken = default)
         {
-            var author = await _repository.GetByIdAsync(id, cancellationToken);
-
-            return _mapper.Map<AuthorResource>(author);
+            var authors = await _repository.GetAuthorsAsync();
+            return _mapper.Map<IEnumerable<IdentityUser>, IEnumerable<AuthorResource>>(authors);
         }
 
-        public async Task<IEnumerable<AuthorResource>> GetAuthors()
+        public async Task<int> CountAuthors(CancellationToken cancellationToken = default)
         {
-            var authors = await _repository.GetAllAsync();
-            return _mapper.Map<IEnumerable<Author>, IEnumerable<AuthorResource>>(authors);
+            return await _repository.CountAuthorsAsync(cancellationToken);
         }
-
-        public async Task<AuthorResource> UpdateAuthor(int id, AddAuthorResource authorResource, CancellationToken cancellationToken = default)
-        {
-            var author = _mapper.Map<Author>(authorResource);
-            author.Id = id;
-            await _repository.UpdateAsync(author, cancellationToken);
-            return await GetAuthor(author.Id, cancellationToken);
-        }
-        
     }
 }
