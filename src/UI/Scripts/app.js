ee0f9b6979c48356bb3dda9fc4c8c1dd6541525c
Restyled.io <commits@restyled.io>
2021-03-23 12:39:02 +0000

    Restyled by prettier

--- src/UI/Scripts/app.js
+++ src/UI/Scripts/app.js
@@ -1,24 +1,22 @@
-var penApp = angular.module('penApp', []);
-penApp.controller('ArticlesController', function($scope, $http, $log) {
+var penApp = angular.module("penApp", []);
+penApp.controller("ArticlesController", function ($scope, $http, $log) {
   $http({
+    method: "GET",
 
-    method : 'GET',
+    url: "http://localhost:5000/api/v1/Articles",
 
-    url : 'http://localhost:5000/api/v1/Articles',
-
-    data : 'articles'
-
-  })
-      .then(
-          function success(response) {
-            // this function will be called when the request is success
-            $scope.articles = response.data;
-            $log.log($scope.articles);
-          },
-          function error(response) {
-            // this function will be called when the request returned error
-            // status
-            $scope.error = response.error;
-            $log.log($scope.error);
-          });
+    data: "articles",
+  }).then(
+    function success(response) {
+      // this function will be called when the request is success
+      $scope.articles = response.data;
+      $log.log($scope.articles);
+    },
+    function error(response) {
+      // this function will be called when the request returned error
+      // status
+      $scope.error = response.error;
+      $log.log($scope.error);
+    }
+  );
 });
