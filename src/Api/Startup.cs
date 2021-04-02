e0a33c65de123510f9ba3bac2a143cb54a87761a
Restyled.io <commits@restyled.io>
2021-04-02 04:30:55 +0000

    Restyled by astyle

--- src/Api/Startup.cs
+++ src/Api/Startup.cs
@@ -8,52 +8,54 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 namespace Api {
-    public class Startup {
-        public Startup (IConfiguration configuration) {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices (IServiceCollection services) {
-            services.InstallServicesInAssembly (Configuration);
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure (IApplicationBuilder app, IWebHostEnvironment env) {
-            if (env.IsDevelopment ()) {
-                app.UseDeveloperExceptionPage ();
-            } else {
-                app.UseHsts ();
-            }
-
-            var swaggerOptions = new SwaggerOptions ();
-            Configuration.GetSection (nameof (SwaggerOptions)).Bind (swaggerOptions);
-
-            app.UseSwagger (options => {
-                options.RouteTemplate = swaggerOptions.JsonRoute;
-
-            });
-
-            app.UseSwaggerUI (options => {
-                options.SwaggerEndpoint (swaggerOptions.UiEndpoint, swaggerOptions.Description);
-            });
-
-            app.UseRouting ();
-
-            app.UseHttpsRedirection ();
-            app.UseStaticFiles ();
-
-            app.UseAuthentication ();
-            app.UseAuthorization ();
-            app.UseEndpoints (endpoints => {
-                endpoints.MapGet ("/", context => {
-                    context.Response.Redirect ("/index.html");
-                    return Task.CompletedTask;
-                });
-                endpoints.MapControllers ();
-            });
-        }
+public class Startup {
+    public Startup (IConfiguration configuration) {
+        Configuration = configuration;
     }
+
+    public IConfiguration Configuration {
+        get;
+    }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices (IServiceCollection services) {
+        services.InstallServicesInAssembly (Configuration);
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure (IApplicationBuilder app, IWebHostEnvironment env) {
+        if (env.IsDevelopment ()) {
+            app.UseDeveloperExceptionPage ();
+        } else {
+            app.UseHsts ();
+        }
+
+        var swaggerOptions = new SwaggerOptions ();
+        Configuration.GetSection (nameof (SwaggerOptions)).Bind (swaggerOptions);
+
+        app.UseSwagger (options => {
+            options.RouteTemplate = swaggerOptions.JsonRoute;
+
+        });
+
+        app.UseSwaggerUI (options => {
+            options.SwaggerEndpoint (swaggerOptions.UiEndpoint, swaggerOptions.Description);
+        });
+
+        app.UseRouting ();
+
+        app.UseHttpsRedirection ();
+        app.UseStaticFiles ();
+
+        app.UseAuthentication ();
+        app.UseAuthorization ();
+        app.UseEndpoints (endpoints => {
+            endpoints.MapGet ("/", context => {
+                context.Response.Redirect ("/index.html");
+                return Task.CompletedTask;
+            });
+            endpoints.MapControllers ();
+        });
+    }
+}
 }
