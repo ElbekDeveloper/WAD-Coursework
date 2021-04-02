72a413364ac0effe782a3a1392e5413665a7b2da
Elbek <52498074+ElbekDeveloper@users.noreply.github.com>
2021-04-02 09:23:50 +0500

    Update endpoints logic

--- src/Api/Startup.cs
+++ src/Api/Startup.cs
@@ -1,72 +1,58 @@
-﻿using Api.Installers;
+﻿using System.Threading.Tasks;
+using Api.Installers;
 using Api.Options;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System.Threading.Tasks;
 
-namespace Api
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
+namespace Api {
+    public class Startup {
+        public Startup (IConfiguration configuration) {
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.InstallServicesInAssembly(Configuration);
+        public void ConfigureServices (IServiceCollection services) {
+            services.InstallServicesInAssembly (Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseHsts();
+        public void Configure (IApplicationBuilder app, IWebHostEnvironment env) {
+            if (env.IsDevelopment ()) {
+                app.UseDeveloperExceptionPage ();
+            } else {
+                app.UseHsts ();
             }
 
-            var swaggerOptions = new SwaggerOptions();
-            Configuration.GetSection(nameof(SwaggerOptions)).Bind(swaggerOptions);
+            var swaggerOptions = new SwaggerOptions ();
+            Configuration.GetSection (nameof (SwaggerOptions)).Bind (swaggerOptions);
 
-            app.UseSwagger(options =>
-            {
+            app.UseSwagger (options => {
                 options.RouteTemplate = swaggerOptions.JsonRoute;
 
             });
 
-            app.UseSwaggerUI(options =>
-            {
-                options.SwaggerEndpoint
-                (swaggerOptions.UiEndpoint, swaggerOptions.Description);
-            }
-            );
+            app.UseSwaggerUI (options => {
+                options.SwaggerEndpoint (swaggerOptions.UiEndpoint, swaggerOptions.Description);
+            });
 
-            app.UseRouting();
+            app.UseRouting ();
 
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
+            app.UseHttpsRedirection ();
+            app.UseStaticFiles ();
 
-            app.UseAuthentication();
-            app.UseAuthorization();
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", context =>
-                {
-                    context.Response.Redirect("/swagger/");
+            app.UseAuthentication ();
+            app.UseAuthorization ();
+            app.UseEndpoints (endpoints => {
+                endpoints.MapGet ("/", context => {
+                    context.Response.Redirect ("/index.html");
                     return Task.CompletedTask;
                 });
-                endpoints.MapControllers();
+                endpoints.MapControllers ();
             });
         }
     }
